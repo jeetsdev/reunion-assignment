159855d8f18f8e4622c60bc9c7dfa64878910ad4
Ranjeet Singh <44586579+jeetsdev@users.noreply.github.com>
2022-09-03 14:07:40 +0530

    feat: routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import { Favorite, Home, NotFound } from "./components/pages";
+import { Header } from "./components";
 
 function App() {
-	return <div className="App">
-		<p>Header</p>
-		<p>Container</p>
-		<p>footer</p>
-	</div>;
+	return (
+		<div className="App">
+			<Header />
+			<Routes>
+				<Route path="*" element={<NotFound />} />
+				<Route path="/" element={<Home />} />
+				<Route path="/favorite" element={<Favorite />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
